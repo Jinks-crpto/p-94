bb94c4d8155c35f474cb0c77be54f30b13e74ffa
Jinks-crpto <77697885+Jinks-crpto@users.noreply.github.com>
2021-07-07 10:12:00 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCX5iNgaskCP7kMgAG0nUDoa0TSX6Qd0J4",
+    authDomain: "wassup-copy.firebaseapp.com",
+    projectId: "wassup-copy",
+    storageBucket: "wassup-copy.appspot.com",
+    messagingSenderId: "29472729468",
+    appId: "1:29472729468:web:adda7b2f80a12043f99cf9",
+    measurementId: "G-YG7KKF2P5R"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
